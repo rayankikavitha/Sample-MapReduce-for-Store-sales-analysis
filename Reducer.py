e498e6d9a486dd61950bdbda5079fb2af2275848
Kavitha Rayanki <rayanki.kavitha@gmail.com>
2017-05-07 23:38:11 -0700

    Add files via upload

---
+++ Reducer.py
@@ -0,0 +1,18 @@
+# print total sales by each store.
+# Input Map data
+import sys
+
+salesTotals = {}
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    store, sale = data
+    salesTotals.setdefault(store, 0)
+    salesTotals[store] += float(sale)
+
+for store in salesTotals:
+    print "{0}\t{1}".format(store, salesTotals[store])
